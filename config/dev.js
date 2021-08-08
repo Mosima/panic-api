12a574dfa455cd96ff1b46768a632f3da7839382
Mosima <mosimagodfrey@gmail.com>
2021-08-08 21:38:13 +0200

    panic-api

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pgDatabase: process.env.PGDATABASE,
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgPort: process.env.PGPORT,
+    pgPass: process.env.PGPASSWORD,
+    secretKey: process.env.COOKIE_KEY,
+    port: process.env.PORT,
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const compression = require('compression');
+const cors = require('cors');
+const async = require('async');
+require('dotenv').config()
+const { Client, Pool } = require('pg')
+
+const secret = require('./config/secret')
+
+const db = require("knex")({
+    client: "pg",
+    connection: {
+      host: secret.pgHost,
+      user: secret.pgUser,
+      password: secret.pgPass,
+      database: secret.pgDatabase
+    }
+  });
+
+const app = express();
+
+app.set("db", db)
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(compression());
+app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.urlencoded({ extended: true, limit: '200mb', parameterLimit: 100000}));
+app.use(morgan('dev'));
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000 ,
+    keys: [secret.secretKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(morgan('dev'));
+app.use(cors(
+    {origin: ['http://localhost:3000', 'http://localhost:3001'], 
+    credentials: true
+}));
+
+
+//new routes
+//require('./routes/post.route')(app, async);
+ 
+
+// app.get('*', (req, res)=>{
+//    //res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
+app.get('/', (req, res) => {
+    res.send("Running")
+  })
+  
+
+app.listen(secret.port, () => console.log(`Node running on server ${secret.port}`));
